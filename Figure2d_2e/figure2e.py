0648673e70f6e9ec22186b5a796378c643811614
chengyu zhang <ton_nom@163.com>
2023-12-03 15:42:31 +0800

    20231203

--- Figure2d_2e/figure2e.py
+++ Figure2d_2e/figure2e.py
@@ -1,49 +1,98 @@
+import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
-# width of the bars
-barWidth = 0.3
+m830 = pd.read_excel("./M_yeastGEM_v8__46__3__46__0_sub_use.xlsx")
+m900 = pd.read_excel("./yeastGEM_develop_sub_use.xlsx")
+dataset = pd.read_table("./Biolog_Substrate.tsv")
 
-# Choose the height of the blue bars
-# bars1 = [88.09, 66]
-#
-# # Choose the height of the cyan bars
-# bars2 = [85.75, 52]
-bars1 = [88.09, 66]
+def source(data, s):
+    new_data = pd.DataFrame(columns=data.columns)
+    for i in data.index:
+        if data.loc[i, 'Substrate_type'] == s:
+            new_data.loc[i, :] = data.loc[i, :]
+    return new_data
 
-# Choose the height of the cyan bars
-bars2 = [85.75, 52]
 
-# The x position of bars
-r1 = np.arange(len(bars1))
-r2 = [x + 1.2*barWidth for x in r1]
+def check_growth(data):
+    c = [data.loc[i, 'Growth_Biolog'] == data.loc[i, 'Growth_Model'] for i in data.index]
+    return np.sum(c)
 
-# Create blue bars
-plt.bar(r2, bars1, width=barWidth, color='#6f9bc6', edgecolor='black', capsize=7, label='yeast 9')
+if __name__ == '__main__':
+    cover8 = []
+    cover9 = []
+    true8 = []
+    true9 = []
+    for s in ['C', 'N', 'P', 'S']:
+        new_data = source(m830, s)
+        ct = check_growth(new_data)
+        cover = len(new_data.index)
+        cover8.append(cover)
+        true8.append(ct)
+        print('In model m830, true prediction using {} is {}'.format(s, ct))
+    for s in ['C', 'N', 'P', 'S']:
+        new_data = source(m900, s)
+        ct = check_growth(new_data)
+        cover = len(new_data.index)
+        cover9.append(cover)
+        true9.append(ct)
+        print('In model m900, true prediction using {} is {}'.format(s, ct))
+    c = n = p = s = 0
+    for da in dataset.index:
+        if dataset.loc[da, 'Substrate_type'] == 'C':
+            c += 1
+        elif dataset.loc[da, 'Substrate_type'] == 'N':
+            n += 1
+        elif dataset.loc[da, 'Substrate_type'] == 'P':
+            p += 1
+        elif dataset.loc[da, 'Substrate_type'] == 'S':
+            s += 1
+    whole = [c, n, p, s]
 
-# Create cyan bars
-plt.bar(r1, bars2, width=barWidth, color='#f3993a', edgecolor='black', capsize=7, label='yeast 8.3')
+################################################################、
+# figure
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import seaborn as sns
+    sou = ['C', 'N', 'P', 'S']
+    barWidth = 0.25
+    r1 = np.arange(len(sou))
+    r2 = [x + 1.2*barWidth for x in r1]
+    r3 = [x + barWidth for x in r2]
+    bottom9 = []
+    for i in range(len(true9)):
+         sum = true9[i] + cover9[i]
+         bottom9.append(sum)
+    bottom8 = []
+    for i in range(len(true9)):
+         sum = true8[i] + cover8[i]
+         bottom8.append(sum)
+    plt.axes().set_facecolor('white')
+    plt.figure(figsize=(8, 6))
+    # plt.bar(r1, true9, width=barWidth, color='#6f9bc6', edgecolor='Black', label='Truly predicted by Yeast 9')
+    # plt.bar(r2, true8, width=barWidth, color='#f3993a', edgecolor='Black', label='Truly predicted by Yeast 8.3')
+    plt.bar(r2, true9, width=barWidth, color='#6f9bc6', edgecolor='Black', label='Truly predicted by Yeast 9')
+    plt.bar(r1, true8, width=barWidth, color='#f3993a', edgecolor='Black', label='Truly predicted by Yeast 8.3')
 
-font1 = {'family': 'Arial',
-         'weight': 'normal',
-         'size': 20,
-         }
-font2 = {'family': 'Arial',
-         'weight': 'normal',
-         'size': 22,
-         }
+    # plt.bar(r1, cover9, width=barWidth, bottom=true9, color='#a4d4b0', edgecolor='white', label='substrates coverd by Yeast9')
+    # plt.bar(r2, cover8, width=barWidth, bottom=true8, color='#f6b683', edgecolor='white', label='substrates coverd by Yeast8')
+    #
+    # plt.bar(r3, whole, width=barWidth, color='#ddedd7', edgecolor='white', label='whole dataset')
+    font2 = {'family': 'Arial',
+             'weight': 'normal',
+             'size': 26,
+             }
+    font3 = {'family': 'Arial',
+             'weight': 'normal',
+             'size': 16,
+             }
 
-# general layout
-plt.xticks([r + 1.2*barWidth/2 for r in range(len(bars1))],
-           ['Gene essentiality', 'Memote total score'],
-           )
-plt.ylabel('Percentage (%)', fontdict=font2)
-plt.yticks(np.arange(0, 100, 10), fontproperties='Arial', size=20)
-plt.xticks(fontproperties='Arial', size=20)
-
-font = {'family': 'Arial', 'weight': 'normal', 'size': 18}
-legend = plt.legend(prop=font)
-# Show graphic
-plt.tight_layout()
-plt.savefig('./memote.tif', dpi=600)
-plt.show()
+    plt.ylabel('Substrate Number', fontdict=font2)
+    plt.xticks([r + 1.2 *barWidth/2 for r in range(len(sou))],
+               ['Carbon', 'Nitrogen', 'Phosphorus', 'Sulfur'],
+               fontproperties='Arial', size=26)
+    plt.yticks(np.arange(0, 55, 10), fontproperties='Arial', size=28)
+    plt.tick_params(top=False, bottom=True, left=True, right=False)
+    plt.legend(prop=font3)
+    plt.tight_layout()
+    plt.savefig('sub_use.tif', dpi=600)
+    plt.show()
